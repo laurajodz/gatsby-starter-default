52976decdb497acf2322c43c3993e63120c083ca
Laura Jodz <laurajodz@gmail.com>
2018-10-13 21:39:01 -0700

    second try for branch1

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi Laura Jodz!</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something hilarious.</p>
+    <p>Now go build something hilarious and unique.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
